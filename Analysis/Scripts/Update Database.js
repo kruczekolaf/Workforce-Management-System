a561e608ab0d04085ce0341e35a2657906188bb2
Olaf Kruczek <olafkruczek@gmail.com>
2025-05-14 15:57:15 +0200

    Update Update Database.js

--- Analysis/Scripts/Update Database.js
+++ Analysis/Scripts/Update Database.js
@@ -1,6 +1,6 @@
 function update_database() {
 
-  const areas_sheet = SpreadsheetApp.openById("1qvOVi5Jk86jJyvtUq4Nh0UBAo0ToaC-Zr-qk780atnw").getSheetByName("Areas");
+  const areas_sheet = SpreadsheetApp.openById("YOUR_WORKAREAS_SPREADSHEET_ID").getSheetByName("Areas");
 
   const last_row = areas_sheet.getlast_row();
   const data_range = areas_sheet.getRange(3, 1, last_row - 2, 14);
@@ -8,7 +8,7 @@ function update_database() {
   data.sort((a, b) => a[0].localeCompare(b[0]));
   data_range.setValues(data);
 
-  const analysis_sheet = SpreadsheetApp.openById("15zWq6frkb-ghUprdpDoKsAjmvpCkRv3rQKngO4jPrH4").getSheetByName("Database");
+  const analysis_sheet = SpreadsheetApp.openById("YOUR_ANALYSIS_SPREADSHEET_ID").getSheetByName("Database");
 
   const employee = data.map(row => row[0]).filter(name => name);
   analysis_sheet.getRange(2, 1, employee.length).setValues(employee.map(name => [name]));
