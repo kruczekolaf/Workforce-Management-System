edf4a5e14c81c54f847e72f16ac94ac7e273fe7f
Olaf Kruczek <olafkruczek@gmail.com>
2025-05-14 15:54:52 +0200

    Update Enter Analysis.js

--- Analysis/Scripts/Enter Analysis.js
+++ Analysis/Scripts/Enter Analysis.js
@@ -1,6 +1,6 @@
 function evaluate_employees() {
 
-  const spreadsheet = SpreadsheetApp.openById("15zWq6frkb-ghUprdpDoKsAjmvpCkRv3rQKngO4jPrH4");
+  const spreadsheet = SpreadsheetApp.openById("YOUR_ANALYSIS_SPREADSHEET_ID");
   const analysis_sheet = spreadsheet.getSheetByName("Analysis");
   const data_sheet = spreadsheet.getSheetByName("Data");
 
