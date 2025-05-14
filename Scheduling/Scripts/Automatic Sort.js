77e5cfeee0f16a5b157b0122c054156b99a00f81
Olaf Kruczek <olafkruczek@gmail.com>
2025-05-14 15:57:53 +0200

    Update Automatic Sort.js

--- Scheduling/Scripts/Automatic Sort.js
+++ Scheduling/Scripts/Automatic Sort.js
@@ -1,6 +1,6 @@
 function automatic_sort() {
 
-  const spreadsheet = SpreadsheetApp.openById("1Hw1RQdfN0TQ1NtSzwc9Qhnp0X_7sprhllYQCXEuuNxo");
+  const spreadsheet = SpreadsheetApp.openById("YOUR_SCHEDULING_SPREADSHEET_ID");
   const database_sheet = spreadsheet.getSheetByName("Database");
 
   database_sheet.getRange("A2:N1000").sort({ column: 1, ascending: true });
