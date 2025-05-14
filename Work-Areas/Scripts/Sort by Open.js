5c6b3675f2a87e1f9d8e4a1a26bb9134e5909739
Olaf Kruczek <olafkruczek@gmail.com>
2025-05-14 16:01:46 +0200

    Update Sort by Open.js

--- Work-Areas/Scripts/Sort by Open.js
+++ Work-Areas/Scripts/Sort by Open.js
@@ -1,6 +1,6 @@
 function automatic_sort() {
 
-  const source_sheet = SpreadsheetApp.openById("1qvOVi5Jk86jJyvtUq4Nh0UBAo0ToaC-Zr-qk780atnw");
+  const source_sheet = SpreadsheetApp.openById("YOUR_WORKAREAS_SPREADSHEET_ID");
   const areas_sheet = source_sheet.getSheetByName("Areas");
 
   areas_sheet.getRange("A3:N1000").sort({column: 1, ascending: true});
